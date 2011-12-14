9c0990b3463c2663cbbe87d495e53cdc21336973
Roman romul.zurov Zhdanov <romul.zurov@gmail.com>
2011-12-14 09:21:31 +0400

    v 0.3.0

--- pyCpuTachometer.py
+++ pyCpuTachometer.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 '''
 TODO:
-- привязку paintEvevn к файлам-картинкам убрать
-- thread!
+- paintEvent: delete link to .png files?
+- thread?
+- popUp menu!
+- change size on double click - OK
 '''
 
 
@@ -12,8 +15,6 @@ from PyQt4 import QtCore, QtGui
 import psutil
 
 
-TACHO_PATH = 'tacho02.256.png'
-ARROW_PATH = 'arrow02.256.png'
 INTERVAL = 100
 MAX_ANGLE = 252.0
 TACHO_BUF_SIZE = 30 * 100 / INTERVAL
@@ -22,59 +23,97 @@ GAZ_BUF_SIZE = 10
 
 class Display(QtGui.QLabel):
     def __init__(self, parent = None):
-        self.k_scale = 1
+        self.main_size = 192
         QtGui.QLabel.__init__(self, parent)
-#        self.setAttribute(QtCore.Qt.WA_NoSystemBackground)
+        self.setAttribute(QtCore.Qt.WA_NoSystemBackground)
         self.setWindowFlags(QtCore.Qt.CustomizeWindowHint | 
                             QtCore.Qt.WindowStaysOnTopHint)
         
-        self.pixmap = QtGui.QPixmap(TACHO_PATH)
-        self.setFixedSize(self.pixmap.width() * self.k_scale,
-                          self.pixmap.height() * self.k_scale)
+        self.set_path()
+        self.pixmap = QtGui.QPixmap(self.tacho_path)
+        self.set_main_size()
         self.timer = QtCore.QTimer(self)
         self.timer.start(INTERVAL)
         self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.foo)
-        self.tacho_angle = self.gaz_angle = 0.0
+        self.tacho_angle = self.gaz_angle = self.mem_angle = 0.0
         self.tacho = self.gaz = 0.0
         self.tacho_arr = self.zabivka(TACHO_BUF_SIZE, 10.0)
         self.gaz_arr = self.zabivka(GAZ_BUF_SIZE, 0.0)
-        self.cx_tacho = self.pixmap.width() / 2
-        self.cy_tacho = self.pixmap.height() / 2
         self.drag_flag = False
-        
-        
+        self.MEM = psutil.TOTAL_PHYMEM
+#        self.painter = QtGui.QPainter(self)
+    
+    
+    
+    
+    def set_path(self):
+        self.tacho_path = 'tacho02.' + str(self.main_size) + '.png'
+        self.arrow_path = 'arrow02.' + str(self.main_size) + '.png'
+    
+    def set_main_size(self):
+        self.set_path()
+        self.cx_tacho = self.main_size / 2
+        self.cy_tacho = self.main_size / 2
+        self.setFixedSize(self.main_size, self.main_size)
+    
+    def reset_main_size(self):
+        self.main_size = (self.main_size - 64)
+        if self.main_size < 64:
+            self.main_size = 256
+        self.set_main_size()
     
     def zabivka(self, size, val):
         ss = []
-        for x in range(size): 
+        for x in range(size):
             ss.append(val)
         return ss
     
-    def draw_arrow(self, painter, cx, cy, angle, dx, dy, path):
-        painter.translate(cx, cy)
-        painter.rotate(angle)
-        painter.translate(-cx, -cy)
-        painter.drawPixmap(dx, dy, QtGui.QPixmap(path))
+    def draw_arrow(self, cx, cy, angle, sc, path):
+        self.painter.translate(cx, cy)
+        self.painter.rotate(angle)
+        self.painter.scale(sc, sc)
+        self.painter.translate(-cx, -cy)
+        self.painter.drawPixmap(0, 0, QtGui.QPixmap(path))
     
-    def paintEvent(self, eve):
-        painter = QtGui.QPainter(self)
-        painter.scale(self.k_scale, self.k_scale)
-        
-#        painter.translate(self.cx_tacho, self.cy_tacho)
-        painter.drawPixmap(0, 0, QtGui.QPixmap(TACHO_PATH))
-        
+    def old_draw_gaz(self, painter):
         pen = QtGui.QPen()
         pen.setWidth(3)
-        pen.setCapStyle(0x20)
+        pen.setCapStyle(QtCore.Qt.RoundCap)
         pen.setColor(QtGui.QColor.fromHsv(100 - self.gaz, 255, 255, 255))
         painter.setPen(pen)
-        gaz_rect = QtCore.QRect(45, 194 - (45 + 104), 104, 104)
-        gaz_start_angle = -16 * 148
-        painter.drawArc(gaz_rect, gaz_start_angle, self.gaz_angle)
+        bru = QtGui.QBrush()
+        bru.setStyle(QtCore.Qt.SolidPattern)
+        bru.setColor(QtGui.QColor.fromHsv(100 - self.gaz, 255, 255, 255))
+        painter.setBrush(bru)
+        kr = 3
+        gaz_rect = QtCore.QRect(self.main_size / 2 - (self.main_size / (kr * 2)),
+                                self.main_size / 2 - (self.main_size / (kr * 2)),
+                                self.main_size / kr , self.main_size / kr)
+        gaz_start_angle = -16 * 90
+        painter.drawPie(gaz_rect, gaz_start_angle, self.gaz_angle)
+        pass
+    
+    def draw_tacho(self):
+        self.draw_arrow(self.cx_tacho, self.cy_tacho, self.tacho_angle,
+                        1.0, self.arrow_path)
+    
+    def draw_gaz(self):
+        self.draw_arrow(self.cx_tacho, self.cy_tacho, self.gaz_angle,
+                        0.64, self.arrow_path)
+    
+    def paintEvent(self, eve):
+        self.painter = QtGui.QPainter(self)
+#        painter = QtGui.QPainter(self)
+        fon = QtGui.QPixmap(self.tacho_path)
+        self.setMask(fon.mask())
+        self.painter.drawPixmap(0, 0, QtGui.QPixmap(self.tacho_path))
         
-        self.draw_arrow(painter, self.cx_tacho, self.cy_tacho, self.tacho_angle, 0, 0, ARROW_PATH)
-        painter.resetTransform()
-        painter.end()
+        self.draw_tacho()
+        self.painter.resetTransform()
+        self.draw_gaz()
+        
+        self.painter.resetTransform()
+        self.painter.end()
     
     
     def usred(self, ss, v):
@@ -88,13 +127,15 @@ class Display(QtGui.QLabel):
     
     def foo(self):
         perc = psutil.cpu_percent(interval = None, percpu = False)
+        mem = psutil.avail_phymem()
         self.gaz_arr, self.gaz = self.usred(self.gaz_arr, perc)
         d = 4;    aa = int(round(self.gaz, 0)) / d;    aa *= d
         self.gaz = aa
         self.tacho_arr, self.tacho = self.usred(self.tacho_arr, self.gaz)
         
         self.tacho_angle = (MAX_ANGLE / 100.0) * self.tacho
-        self.gaz_angle = -16 * (MAX_ANGLE / 100.0) * self.gaz
+        self.gaz_angle = (MAX_ANGLE / 100.0) * self.gaz
+        self.mem_angle = MAX_ANGLE * (1.0 * mem / self.MEM)
         self.update()
         pass
     
@@ -114,7 +155,10 @@ class Display(QtGui.QLabel):
     def mouseReleaseEvent(self, event):
         if (event.button() == QtCore.Qt.LeftButton):
             self.drag_flag = False
-
+    
+    def mouseDoubleClickEvent (self, event):
+        if (event.button() == QtCore.Qt.LeftButton):
+            self.reset_main_size()
 
 
 if __name__ == '__main__':
@@ -124,4 +168,4 @@ if __name__ == '__main__':
     win = Display()
     win.show()
     sys.exit(app.exec_())
-    
+
